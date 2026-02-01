a37986a4d545cb0b11d2e99c2e05cb386f6584ef
SandileStainlyNdlovu <stainlysnd@gmail.com>
2026-02-01 12:39:06 +0200

    Update app_profilerSSN_menus.py

--- app_profilerSSN_menus.py
+++ app_profilerSSN_menus.py
@@ -48,8 +48,8 @@ if menu == "Researcher Profile":
     st.write(f"**Institution:** {institution}")
     
     st.image(
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-    caption="Nature (Pixabay)"
+    "Website_logo.jpg",
+    caption="at the heart of it all"
 )
 
 elif menu == "Publications":
@@ -134,3 +134,4 @@ elif menu == "Contact":
     email = "jane.doe@example.com"
 
     st.write(f"You can reach me at {email}.")
+
